65a0051c5388cd4320dcb7babd1233a5b17efe8c
Gabriel Mejia <endermejia032@gmail.com>
2020-04-08 01:22:22 +0200

    generate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ControlComponent} from './components/control/control.component';
+import {SettingsComponent} from './components/settings/settings.component';
+import {StatusComponent} from './components/status/status.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'status', component: StatusComponent
+  },
+  {
+    path: 'control', component: ControlComponent
+  },
+  {
+    path: 'settings', component: SettingsComponent
+  },
+  {
+    path: '', component: StatusComponent
+  },
+  {
+    path: '**', component: StatusComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
