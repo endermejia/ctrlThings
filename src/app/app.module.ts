7b195d2343144e2671462f87f5ac7fb395940302
Gabriel Mejia <endermejia032@gmail.com>
2020-04-08 00:39:56 +0200

    registerLocaleDate spanish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es'
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
