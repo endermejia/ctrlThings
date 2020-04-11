1a54f98bba596f5ca7dd5be1582248c004c6c9eb
Gabriel Mejia <endermejia032@gmail.com>
2020-04-11 23:17:06 +0200

    MQTT changes: First sent and received msg, but no connection with Rasp

--- src/app/components/status/status.component.ts
+++ src/app/components/status/status.component.ts
@@ -1,5 +1,4 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {log} from 'util';
+import {Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {IMqttMessage, MqttService} from 'ngx-mqtt';
 import {Subscription} from 'rxjs';
 
@@ -9,43 +8,30 @@ import {Subscription} from 'rxjs';
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.scss']
 })
-export class StatusComponent implements OnInit, OnDestroy {
-private subscription: Subscription;
-  topicname: any;
-  msg: any;
+export class StatusComponent implements OnDestroy {
+  // private mqttClient: Paho.MQTT.Client;
+  private subscription: Subscription;
+  private message: string;
+  topicname = '/rasp/led';
+  msg: string;
   isConnected: boolean = false;
-@ViewChild('msglog', { static: true }) msglog: ElementRef;
+  @ViewChild('msglog', {static: true}) msglog: ElementRef;
 
-  constructor(private _mqttService: MqttService) { }
-
-  ngOnInit(): void {}
+  constructor(private _mqttService: MqttService) {
+    this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
+      this.message = message.payload.toString();
+      console.log('Received MQTT: ', this.message);
+    });
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-
-  subscribeNewTopic(): void {
-    console.log('inside subscribe new topic')
-    this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
-      this.msg = message;
-      console.log('msg: ', message)
-      this.logMsg('Message: ' + message.payload.toString() + '<br> for topic: ' + message.topic);
-    });
-    this.logMsg('subscribed to topic: ' + this.topicname)
-  }
-
-  sendmsg(): void {
+  public unsafePublish(): void {
     // use unsafe publish for non-ssl websockets
-    this._mqttService.unsafePublish(this.topicname, this.msg, { qos: 1, retain: true })
+    console.log('Sent MQTT: ', this.msg);
+    this._mqttService.unsafePublish(this.topicname, this.msg, {qos: 1, retain: true})
     this.msg = ''
-  }
-
-  logMsg(message): void {
-    this.msglog.nativeElement.innerHTML += '<br><hr>' + message;
-  }
-
-  clear(): void {
-    this.msglog.nativeElement.innerHTML = '';
   }
 }
