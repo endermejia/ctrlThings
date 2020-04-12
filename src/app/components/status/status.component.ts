a8d1c68ebcd0f42ba76f02acebe6bb87183861f0
Gabriel Mejia <endermejia032@gmail.com>
2020-04-12 13:47:26 +0200

    changes in mqtt first version

--- src/app/components/status/status.component.ts
+++ src/app/components/status/status.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {IMqttMessage, MqttService} from 'ngx-mqtt';
 import {Subscription} from 'rxjs';
 
@@ -9,16 +9,13 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./status.component.scss']
 })
 export class StatusComponent implements OnDestroy {
-  // private mqttClient: Paho.MQTT.Client;
   private subscription: Subscription;
-  private message: string;
-  topicname = '/rasp/led';
+  message: string;
+  topicName = '/rasp/led';
   msg: string;
-  isConnected: boolean = false;
-  @ViewChild('msglog', {static: true}) msglog: ElementRef;
 
   constructor(private _mqttService: MqttService) {
-    this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
+    this.subscription = this._mqttService.observe(this.topicName).subscribe((message: IMqttMessage) => {
       this.message = message.payload.toString();
       console.log('Received MQTT: ', this.message);
     });
@@ -31,7 +28,7 @@ export class StatusComponent implements OnDestroy {
   public unsafePublish(): void {
     // use unsafe publish for non-ssl websockets
     console.log('Sent MQTT: ', this.msg);
-    this._mqttService.unsafePublish(this.topicname, this.msg, {qos: 1, retain: true})
+    this._mqttService.unsafePublish(this.topicName, this.msg, {qos: 1, retain: true})
     this.msg = ''
   }
 }
