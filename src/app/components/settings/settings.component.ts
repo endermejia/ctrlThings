bab2675f415b7ea68e1a1059ea9d08e9d0b2d42f
Gabriel Mejia <endermejia032@gmail.com>
2020-04-13 12:56:39 +0200

    settings component update

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MqttService} from 'ngx-mqtt';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss' ],
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _mqttService: MqttService) {
+    this._mqttService.unsafePublish('/rasp/reboot', 'false', {qos: 1, retain: true});
+    this._mqttService.unsafePublish('/rasp/shutdown', 'false', {qos: 1, retain: true});
+  }
 
   ngOnInit(): void {
+  }
+
+  rebootRasp(): void {
+    console.log('Reboot Raspberry');
+    this._mqttService.unsafePublish('/rasp/reboot', 'true', {qos: 1, retain: false});
+  }
+
+  shutdownRasp(): void {
+    console.log('Shutdown Raspberry');
+    this._mqttService.unsafePublish('/rasp/shutdown', 'true', {qos: 1, retain: false});
+
   }
 
 }
